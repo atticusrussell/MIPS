784341726bab26af079a4d6f75e510c574707fa2
atticus <atticusrussell@gmail.com>
2023-06-12 13:51:40 -0400

    implementation from lab 7

---
+++ ips/clk_wiz_0/clk_wiz_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Mon Jun 12 10:57:23 2023
+// Host        : AJR-L5Pro running 64-bit Ubuntu 20.04.5 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/atticus/github/dsd2/dsd2-lab07-MIPS-frequency/ips/clk_wiz_0/clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_wiz_0(clk_out1, reset, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,reset,clk_in1" */;
+  output clk_out1;
+  input reset;
+  input clk_in1;
+endmodule
